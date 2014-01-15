c0c21276d9942eba414f2f28f3d3f5ec4a10c243
jtmkrueger <johnk@linux.com>
2014-01-15 09:19:44 -0700

    version bump

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject com.existentialmutt/lt-ruby "0.0.1"
+(defproject com.existentialmutt/lt-ruby "0.0.4"
   :description "Ruby language plugin for Light Table"
   :dependencies [[org.clojure/clojure "1.5.1"]])
