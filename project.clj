4839ed8c9f6123e9a4e249bee5489f292e234d1b
Rafe Rosen <rrosen@greenriver.org>
2016-02-12 23:08:24 -0500

    version bump in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.existentialmutt/lt-ruby "0.0.15"
+(defproject com.existentialmutt/lt-ruby "0.0.16"
   :description "Ruby language plugin for Light Table"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [crate "0.2.5"]
